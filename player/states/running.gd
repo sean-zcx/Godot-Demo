77c1ac918c2fd4a97920c81c722a3e260986bbf0
Chenxiang Zhang <110497590+Chenxiang-Zhang-PAX@users.noreply.github.com>
2025-07-14 18:09:33 -0400

    anim update
    anim update

--- player/states/running.gd
+++ player/states/running.gd
@@ -1,32 +1,34 @@
 extends PlayerState
 
-@export var RUN_SPEED:float = 300
-@export var RUN_ACCELECTION:float = 1000
-@export var RUN_TURNING_SPEED:float = RUN_ACCELECTION / 10
+
 
 func enter(previous_state_path: String, data := {}) -> void:
-	owner.jump_count = 0
-	player.change_collision_shape('running')
-	player.animation_player.play("running")
+	#owner.jump_count = 0
+	player.change_collision_shape('running_begin')
+	player.animation_player.play("running_begin")
+	#player.animation_player.animation_finished.connect(on_animation_finished)
 
 func physics_process(delta: float) -> void:
 	var input_direction_x := Input.get_axis("move_left", "move_right")
-	#player.velocity.x = player.speed * input_direction_x
-	#player.velocity.y += player.gravity * delta
-	#
-	#player.velocity.x = move_toward(player.velocity.x, RUN_SPEED * player.direction.x, RUN_ACCELECTION * delta)
+	player.velocity.x = move_toward(player.velocity.x, player.RUN_SPEED * input_direction_x, player.RUN_ACCELECTION * delta)
 	
-	
+	if Input.is_action_just_released("move_left") or Input.is_action_just_released("move_right"):
+		finished.emit(RUNNING_END)
 	if not player.is_on_floor():
 		#finished.emit(FALLING)
 		pass
 	elif Input.is_action_just_pressed("jump"):
 		finished.emit(JUMPING)
-	elif is_zero_approx(input_direction_x):
-		#finished.emit(BREAKING)
-		finished.emit(IDLE)
-	else:
-		player.velocity.x = move_toward(player.velocity.x, RUN_SPEED * input_direction_x, RUN_ACCELECTION * delta)
-		if abs(player.velocity.x) > RUN_TURNING_SPEED:
-			player.animation_player.play("run")
+	
+	#elif is_zero_approx(input_direction_x):
+		#finished.emit(IDLE)
+		
+
 	player.move_and_slide()
+
+
+func on_animation_finished(anim_name):
+	if anim_name == "running_begin":
+		print("running_begin finished")
+		player.change_collision_shape('running')
+		player.animation_player.play("running")
