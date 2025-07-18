3b508901564458b780b63fb4f1f63d59f388c7b8
Chenxiang Zhang <110497590+Chenxiang-Zhang-PAX@users.noreply.github.com>
2025-07-18 17:38:20 -0400

    step back 1

--- player/states/idle.gd
+++ player/states/idle.gd
@@ -13,12 +13,23 @@ func physics_process(delta: float) -> void:
 	
 	if !player.is_on_floor():
 		finished.emit(FALLING)
-	
+		return
 	elif Input.is_action_just_pressed("jump"):
 		finished.emit(JUMPING)
+		return
 	if input_direction and player.look_direction_changed:
 		print("[idle.gd] detected direction changed ")
 		finished.emit(TURNING)
 		return
-	elif input_direction.x:
+	if input_direction.x:
 		finished.emit(RUNNING)
+		return
+	if Input.is_action_just_pressed("step_back"):
+		player.velocity.x = move_toward(600 , 0, player.RUN_ACCELECTION * delta)
+		player.animation_player.play("stepping_back")
+
+
+func on_animation_finished(anim_name):
+	if anim_name == "stepping_back":
+		print("stepping_back finished")
+		player.animation_player.play("idle_1")
